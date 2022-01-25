3bf45dd496be26b278e60686dd3b54a59da36382
Larissa Rabelo <56235344+larissarabelo@users.noreply.github.com>
2022-01-25 10:03:41 -0300

    Update time_stretch.py

--- time_stretch.py
+++ time_stretch.py
@@ -4,8 +4,8 @@ from IPython.display import Audio
 y, sr=librosa.load("audio.wav", sr=None)
 
 y_shorter=librosa.effects.time_stretch(y,1.11)
-#shows the audio 11% shorter
+#it shows the audio 11% shorter
 Audio(y_shorter, rate=sr)
 y_longer=librosa.effects.time_stretch(y,0.89)
-#shows the audio 11% longer 
+#it shows the audio 11% longer 
 Audio(y_longer, rate=sr)
